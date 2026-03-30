1f053ccfa79fc4c2304ff8be35411d4a483ec0d5
Akbar Ali <m.akberali507@gmail.com>
2026-03-30 15:17:23 +0500

    feat: implement authentication routes and views for sign-in and sign-out, add profile page with user details

--- Backend/10. Authentication and Authorization with mongoose/app.js
+++ Backend/10. Authentication and Authorization with mongoose/app.js
@@ -4,10 +4,13 @@ const app = express();
 const path = require("path");
 const userModel = require("./models/user");
 const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser())
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -22,7 +25,7 @@ app.post("/sign-up", (req, res) => {
     const { name, email, password, age } = req.body;
 
     bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(password, salt,  async (err, hash) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
             await userModel.create({
                 name,
                 email,
@@ -33,6 +36,46 @@ app.post("/sign-up", (req, res) => {
     })
 
     res.redirect("/sign-up");
+})
+
+app.get("/sign-out", (req, res) => {
+    res.clearCookie("token");
+
+    res.redirect("/");
+})
+
+app.get("/sign-in", (req, res) => {
+    res.render("sign-in");
+})
+
+app.post("/sign-in", async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) return res.send("Invalid Credentials (No User found) :(");
+
+    const isCorrectPassword = await bcrypt.compare(password, user.password);
+
+    if(!isCorrectPassword) return res.send("Invalid Credentials (Incorrect password) :(");
+
+    let name = user.name;
+
+    const token = jwt.sign({ name, email}, "Secret");
+
+    res.cookie("token", token);
+
+    res.redirect("/");
+})
+
+app.get("/profile", (req, res) => {
+    const token = req.cookies.token;
+    if(!token) return res.send("Sign in First");
+    
+    const { name, email } = jwt.verify(token, "Secret");
+
+    res.render("profile", {name, email});
+    res.render("profile");
 })
 
 app.listen(3000);
